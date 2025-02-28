a19b0198845f4f36f7701365e08837dae5a0b1a4
maocq <johncarm@bancolombia.com.co>
2025-02-28 16:43:12 -0500

    Batch

---
+++ infrastructure/entry-points/batch/src/main/java/co/com/bancolombia/batch/ApplicationRunner.java
@@ -0,0 +1,28 @@
+package co.com.bancolombia.batch;
+
+/*
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Log
+@Component
+ */
+public class ApplicationRunner /*implements CommandLineRunner*/ {
+
+    /*
+    private final String batchOperation;
+
+    public ApplicationRunner(@Value("${batch.operation}") String batchOperation) {
+        this.batchOperation = batchOperation;
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("Starting batch" + 10 / 0);
+        System.out.println(batchOperation);
+        log.info("\n\n\nEnd!!!\n\n\n");
+    }
+     */
+}
